8758d551c2f0b2df598d23ae696686d367b67930
Maggie Du <maggie.du@sas.com>
2023-04-14 09:34:43 -0400

    module4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -39,6 +39,7 @@ class MarkdownParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
 class ReStructuredTextParser(Parser):
     file_exts = [".rst"]
     def parse(self, path, source, dest):
@@ -49,3 +50,4 @@ class ReStructuredTextParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
